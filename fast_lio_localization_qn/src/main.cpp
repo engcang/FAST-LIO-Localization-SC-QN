354eba571bd8e38f4b3baa7a82acc4107621e0a1
engcang <engcang@naver.com>
2024-10-28 08:41:50 +0900

    Accepting https://github.com/engcang/FAST-LIO-SAM-QN/pull/7 and https://github.com/engcang/FAST-LIO-SAM-QN/pull/9

--- fast_lio_localization_qn/src/main.cpp
+++ fast_lio_localization_qn/src/main.cpp
@@ -1,24 +1,15 @@
-#include "main.h"
-#include <csignal>
-
-void SigHandle(int sig)
-{
-    std::cout << "You pressed Ctrl + C, exiting" << std::endl;
-    exit(1);
-}
+#include "fast_lio_localization_sc_qn.h"
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "fast_lio_localization_sc_qn_node");
-  ros::NodeHandle nh_private("~");
+    ros::init(argc, argv, "fast_lio_localization_sc_qn_node");
+    ros::NodeHandle nh_private("~");
 
-  FastLioLocalizationScQnClass FastLioLocalizationScQn_(nh_private);
+    FastLioLocalizationScQn FastLioLocalizationScQn_(nh_private);
 
-  signal(SIGINT, SigHandle);
+    ros::AsyncSpinner spinner(3); // Use multi threads
+    spinner.start();
+    ros::waitForShutdown();
 
-  ros::AsyncSpinner spinner(3); // Use multi threads
-  spinner.start();
-  ros::waitForShutdown();
- 
-  return 0;
+    return 0;
 }
